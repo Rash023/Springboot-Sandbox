62088d188f6b2d250d6e8b375b35f4008a807092
Rash023 <rashidmazhar0507@gmail.com>
2025-02-24 13:27:00 +0530

    Added first project

---
+++ first-project/src/main/java/com/example/firstProject/myClass.java
@@ -0,0 +1,12 @@
+package com.example.firstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myClass {
+    @GetMapping("abc")
+    public String sayHello(){
+        return "Hello";
+    }
+}
