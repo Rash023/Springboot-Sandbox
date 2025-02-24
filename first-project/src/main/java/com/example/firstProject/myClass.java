44f890e74e6ed32049cd9b2e65ad3a621f5cfb28
Rash023 <rashidmazhar0507@gmail.com>
2025-02-24 13:43:32 +0530

    Added annotations Component,Autowired etc

--- first-project/src/main/java/com/example/firstProject/myClass.java
+++ first-project/src/main/java/com/example/firstProject/myClass.java
@@ -1,12 +1,17 @@
 package com.example.firstProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class myClass {
+
+    @Autowired
+    private Dog dog;
+
     @GetMapping("abc")
     public String sayHello(){
-        return "Hello";
+        return dog.fun();
     }
 }
