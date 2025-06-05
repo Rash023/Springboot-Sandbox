5ba643b729b5f70e41e1d24ec98a1531dc9d84ea
Rash023 <rashidmazhar0507@gmail.com>
2025-06-05 12:13:50 +0530

    Implemented Google/Github login using OAuth

---
+++ SpringOAuth2Ex/src/main/java/com/rash/SpringOAuth2Ex/SpringOAuth2ExApplication.java
@@ -0,0 +1,13 @@
+package com.rash.SpringOAuth2Ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuth2ExApplication.class, args);
+	}
+
+}
